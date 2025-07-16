842d3b51c9b0a17bb6d10d8a36e11bd1e0c427c5
nayoung.kim <jenny6816@naver.com>
2025-07-17 03:58:23 +0900

    💄 [Design] 이미지 프레임 그림자 조정

--- Divary/Sources/DiaryImageDeco/DiaryImageFrame.swift
+++ Divary/Sources/DiaryImageDeco/DiaryImageFrame.swift
@@ -24,45 +24,54 @@ struct DiaryImageFrame: View {
     
     var body: some View {
         if viewModel.frameType != .origin {
-            if viewModel.isSelected { // 꾸민 사진 띄우기
-                VStack {
-                    Image("testImage")
-                        .resizable()
-                        .scaledToFill()
-                        .frame(width: 230, height: 230)
-                        .clipped()
-                        .cornerRadius(viewModel.frameType.innerCornerRadius)
-                        .padding(.top, 15)
-                        .padding(.bottom, 16)
-                
-                    VStack(alignment: .leading, spacing: 4) {
-                        TextField("캡션 추가...", text: $viewModel.imageCaption)
-                            .foregroundColor(.black)
-                        
-                        Text(viewModel.imageDate)
-                            .foregroundColor(Color(.G_700))
-                            .padding(.bottom, 18)
-                    }
-                }
-                .frame(width: 240)
-                .padding(.horizontal, 10)
-                .background(viewModel.frameType.frameColor)
-                .shadow(color: .black.opacity(0.25), radius: 2, x: 0, y: 4)
-            }
-            else { // frameSelectBar에 프레임 버튼들
-                VStack {
+            if viewModel.isSelected {
+                ZStack {
                     Rectangle()
-                        .fill(Color.white)
-                        .cornerRadius(viewModel.frameType.innerCornerRadius)
-                        .aspectRatio(1, contentMode: .fit)
-                    Spacer()
-                        .frame(height: 20)
+                        .fill(viewModel.frameType.frameColor)
+                        .shadow(color: .black.opacity(0.25), radius: 2, x: 0, y: 4)
+                    VStack {
+                        Image("testImage")
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 230, height: 230)
+                            .clipped()
+                            .cornerRadius(viewModel.frameType.innerCornerRadius)
+                            .padding(.top, 15)
+                            .padding(.bottom, 16)
+
+                        VStack(alignment: .leading, spacing: 4) {
+                            TextField("캡션 추가...", text: $viewModel.imageCaption)
+                                .foregroundColor(.black)
+
+                            Text(viewModel.imageDate)
+                                .foregroundColor(Color(.G_700))
+                                .padding(.bottom, 18)
+                        }
+                    }
+                    .frame(width: 240)
+                    .padding(.horizontal, 10)
                 }
-                .frame(width: 60)
-                .padding(.horizontal, 3)
-                .padding(.top, 3)
-                .background(viewModel.frameType.frameColor)
-                .shadow(color: .black.opacity(0.25), radius: 2, x: 0, y: 4)
+                .frame(width: 230, height: 230)
+            }
+            
+            else { // frameSelectBar에 프레임 버튼들
+                ZStack {
+                    Rectangle()
+                        .fill(viewModel.frameType.frameColor)
+                        .shadow(color: .black.opacity(0.25), radius: 2, x: 0, y: 4)
+
+                    VStack {
+                        RoundedRectangle(cornerRadius: viewModel.frameType.innerCornerRadius)
+                            .fill(Color.white)
+                            .cornerRadius(viewModel.frameType.innerCornerRadius)
+                            .aspectRatio(1, contentMode: .fit)
+                        Spacer()
+                            .frame(height: 20)
+                    }
+                    .padding(.horizontal, 3)
+                    .padding(.top, 3)
+                }
+                .frame(width: 70, height: 85)
             }
         }
         
