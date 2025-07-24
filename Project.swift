cb777be8f354dae94f8f3c59048d85c65395cb52
haeun030 <haeun14_0_0@naver.com>
2025-07-24 14:23:12 +0900

    🐛 Fix: Project.swift package

--- Project.swift
+++ Project.swift
@@ -3,17 +3,14 @@ import ProjectDescription
 let project = Project(
     name: "Divary",
     packages: [
-        .package(url: "https://github.com/Moya/Moya.git", .upToNextMajor(from: "15.0.0")),
-        .package(url: "https://github.com/google/GoogleSignIn-iOS", from: "7.0.0"),
-        .package(url: "https://github.com/danielsaidi/RichTextKit.git", from: "1.2.0")
+        .package(url: "https://github.com/Moya/Moya.git", .upToNextMinor(from: "15.0.3")),
+        .package(url: "https://github.com/google/GoogleSignIn-iOS", .upToNextMinor(from: "7.0.0")),
+        .package(url: "https://github.com/danielsaidi/RichTextKit.git", .upToNextMinor(from: "1.2.0"))
     ],
     settings: .settings(
         base: [
                "GOOGLE_CLIENT_ID": "$(GOOGLE_CLIENT_ID)",
                "GOOGLE_URL_SCHEME": "$(GOOGLE_URL_SCHEME)"
-        ],
-        configurations: [
-            .debug(name: "SecretOnly", xcconfig: .relativeToRoot("../divary-iOS/Configuration/Secret.xcconfig"))
         ]
     ),
     targets: [
@@ -29,10 +26,8 @@ let project = Project(
                         "UIColorName": "",
                         "UIImageName": "",
                     ],
-                    // Secret.xcconfig에서 가져올 값들
                     "API_URL": "$(API_URL)",
                     "GOOGLE_CLIENT_ID": "$(GOOGLE_CLIENT_ID)",
-                    //Infoplist에 정의할 값
                     "CFBundleURLTypes": [
                         [
                             "CFBundleURLSchemes": [
@@ -43,12 +38,12 @@ let project = Project(
                 ]
             ),
             sources: ["Divary/Sources/**"],
-            resources: ["Divary/Resources/**", ".github/**/*"],
+            resources: ["Divary/Resources/**"], // ✅ .github 제거
             dependencies: [
                 .package(product: "Moya"),
                 .package(product: "GoogleSignIn"),
-                .package(product: "GoogleSignInSwift"), // SwiftUI 사용 시에만 필요
-                .package(product: "RichTextKit")
+                .package(product: "GoogleSignInSwift"), // 선택적
+                .package(product: "RichTextKit")        // 실제 product명이 맞는지 꼭 확인
             ]
         ),
         .target(
