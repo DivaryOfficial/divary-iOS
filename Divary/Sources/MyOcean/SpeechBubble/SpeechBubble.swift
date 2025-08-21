b7dd14ce25044d43ec344bff2275843914276292
Park hyeon gyu(바견규) <qwqwqw4756@gmail.com>
2025-08-22 05:23:10 +0900

    ✨[Feat] 나의 바다 뷰 부유 효과 및 말풍선 추가

--- Divary/Sources/MyOcean/SpeechBubble/SpeechBubble.swift
+++ Divary/Sources/MyOcean/SpeechBubble/SpeechBubble.swift
@@ -16,16 +16,36 @@ struct SpeechBubbleView: View {
     let isStoreView: Bool
     let viewModel: CharacterViewModel  // viewModel 추가
     
+    // 부유 애니메이션 상태
+    @State private var floatingOffset: CGFloat = 0
+    
     var body: some View {
-        if customization.speechBubble != .none {
-            if isStoreView {
-                customization.speechBubble.inputView(text: viewModel.speechTextBinding)
-                    .scaleEffect(scale)
-                    .offset(x: (-100 * scale) + x, y: (-170 * scale) + y)
-            } else {
-                customization.speechBubble.view(text: customization.speechText ?? "")
-                    .scaleEffect(scale)
-                    .offset(x: (-100 * scale) + x, y: (-170 * scale) + y)
+        Group {
+            if customization.speechBubble != .none {
+                if isStoreView {
+                    customization.speechBubble.inputView(text: viewModel.speechTextBinding)
+                        .scaleEffect(scale)
+                        .offset(x: (-100 * scale) + x, y: (-170 * scale) + y + floatingOffset)
+                } else {
+                    customization.speechBubble.view(text: customization.speechText ?? "")
+                        .scaleEffect(scale)
+                        .offset(x: (-100 * scale) + x, y: (-170 * scale) + y + floatingOffset)
+                }
+            }
+        }
+        .task {
+            startFloatingAnimation()
+        }
+    }
+    
+    private func startFloatingAnimation() {
+        // 1초 지연 후 시작 (다른 요소들과 다른 타이밍)
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+            withAnimation(
+                Animation.easeInOut(duration: 2.8)
+                    .repeatForever(autoreverses: true)
+            ) {
+                floatingOffset = -12
             }
         }
     }
