f62e838825daae0321f971d125fb5f9a478c5fd2
nayoung.kim <jenny6816@naver.com>
2025-11-27 11:43:24 +0900

    [Fix] 총 다이빙 횟수 오탈자 수정

--- Divary/Sources/Login/LoginView.swift
+++ Divary/Sources/Login/LoginView.swift
@@ -52,7 +52,7 @@ struct LoginView: View {
                     VStack {
                         Spacer()
                         Button(action:{
-                            KeyChainManager.shared.save("eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI0Iiwicm9sZSI6IlVTRVIiLCJ0eXBlIjoiYWNjZXNzIiwiaWF0IjoxNzYzMDUyMDE5LCJleHAiOjE3NjMwNTI5MTl9.MYrFpB7TcgzYWLWnPXHPR_qe0RV79QDTYUsVI8ULus8", forKey: KeyChainKey.accessToken)
+                            KeyChainManager.shared.save("eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI0Iiwicm9sZSI6IlVTRVIiLCJ0eXBlIjoiYWNjZXNzIiwiaWF0IjoxNzY0MjA5MTc2LCJleHAiOjE3NjQyMTAwNzZ9.0ymLxARJUf76PJXIEyMPAUm9YV3xPhN424Dh_alluaQ", forKey: KeyChainKey.accessToken)
                             container.router.push(.MainTabBar)
                         }) {
                             Text("테스트 계정")
